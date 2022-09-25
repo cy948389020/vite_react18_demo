6a47f51168e8844192510858236d3f63e4c0bbe4
chenxiaoyu <chenyu@linhuiba.com>
2022-09-25 22:43:01 +0800

    feat: x

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
 import App from './App'
+import zhCN from 'antd/es/locale/zh_CN'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <ConfigProvider locale={zhCN}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ConfigProvider>
+  </BrowserRouter>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteEslint from 'vite-plugin-eslint'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteEslint()]
+  plugins: [
+    react(),
+    viteEslint(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          libDirectory: 'es',
+          style: (name) => `antd/es/${name}/style`
+        }
+      ]
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  }
 })
